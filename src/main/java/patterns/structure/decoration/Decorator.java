80905241b6d695763b63622040d824a3ded672d8
Ding yubin <yubin.ding@casstime.com>
2021-07-13 14:09:24 +0800

    更新环境配置文件

--- src/main/java/patterns/structure/decoration/Decorator.java
+++ src/main/java/patterns/structure/decoration/Decorator.java
@@ -1,7 +1,7 @@
 package patterns.structure.decoration;
 
 public class Decorator extends Component{
-    private Component component;
+    protected Component component;
     //通过构造函数传递被修饰者
     public Decorator(Component component){
         this.component = component;
